50fc9b78689b32d49ab97e20b48978ea8a6b9fe5
Justin <Justin@Justin-PC.attlocal.net>
2016-10-08 18:56:33 -0700

    Initial commit to repository

---
+++ PrivateEye/src/PrivateEye.java
@@ -0,0 +1,29 @@
+
+import byui.cit260.privateEye.Player;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Justin
+ */
+public class PrivateEye {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        playerOne.setName("Fred Flinstone");
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+    }
+    
+}
